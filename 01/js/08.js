b21ce5c2b6b4e7a01da32d4e9115abd906581c13
Foccy <pouneun@naver.com>
2022-12-26 14:35:37 +0900

    식별자,연산자, 리터럴

--- 01/js/08.js
+++ 01/js/08.js
@@ -1,7 +1,7 @@
-// 논리자료형 : 참or 거짓 
-let a=true;
+// 논리자료형 : 참or 거짓
+let a = true;
 //a="true"
-a=false;
-alert(a)
+a = true;
+alert(a);
 // 자료형을 숫자로 바꿔주는 함수 Number()
-document.write(Number(a))
+document.write(Number(a));
